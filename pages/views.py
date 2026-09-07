09cb8ca0824708666133257c0e0c8ccd5979a130
Wesley Cox <wacox1@buffs.wtamu.edu>
2026-09-07 13:16:04 -0600

    Display the list on the home page

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+tasks = [
+    {"title": "Finish Week 1", "done": True},
+    {"title": "Get an Oil Change", "done": True},
+    {"title": "Respond to Emails", "done": False},
+    {"title": "Pay Rent", "done": True},
+    {"title": "Buy a Lamborghini", "done": False},
+]
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {"tasks": tasks})
 def about(request):
     return render(request, "about.html")
 def contact(request):
